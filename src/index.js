5f859ecf8cade2ab6f35dadf1432e7a8cc788556
Rahul Lingwal <rahullingwal200@gmail.com>
2026-07-17 21:58:58 +0530

    feat: start server only after successful MongoDB connection

--- src/index.js
+++ src/index.js
@@ -7,4 +7,14 @@ import connectDB from "./db/database.js";
 
 dotenv.config({ path: "./.env" });
 
-connectDB();
+const port = process.env.PORT || 5000;
+
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server started at port : ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed ! ", error);
+  });
